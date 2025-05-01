9f1f0143ded61a853eea15877dc2bc9e2cfbbccc
SyedaFarheen1 <spongheen@gmail.com>
2025-05-01 17:43:10 +0500

    made requested change

--- github-example/main.cpp
+++ github-example/main.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
+	string name;
 	cout << "Hello, World!" << endl;
 	cout << "Hello, GitHub!" << endl;
-	cout << "This is a change" << endl;
 
-	string name;
+	cout << "This is a change" << endl;
 	cout << "Enter your name" << endl;
 	cin >> name;
 	
