ba37509f07caec924c7563f40bf4705590b6985b
SyedaFarheen1 <spongheen@gmail.com>
2025-05-01 17:39:51 +0500

    added input feature

--- github-example/main.cpp
+++ github-example/main.cpp
@@ -5,5 +5,10 @@ int main() {
 	cout << "Hello, World!" << endl;
 	cout << "Hello, GitHub!" << endl;
 	cout << "This is a change" << endl;
+
+	string name;
+	cout << "Enter your name" << endl;
+	cin >> name;
+	
 	return 0;
 }
