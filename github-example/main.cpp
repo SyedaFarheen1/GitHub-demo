f190d734d301dd18ff21298641766583a473cb60
SyedaFarheen1 <spongheen@gmail.com>
2025-05-01 18:07:25 +0500

    removed extra stuff

--- github-example/main.cpp
+++ github-example/main.cpp
@@ -5,10 +5,6 @@ int main() {
 	string name;
 	cout << "Hello, World!" << endl;
 	cout << "Hello, GitHub!" << endl;
-
-	cout << "This is a change" << endl;
-	cout << "Enter your name" << endl;
-	cin >> name;
 	
 	return 0;
 }
