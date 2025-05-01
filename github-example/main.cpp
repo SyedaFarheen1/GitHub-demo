8b99740d524dd6900fa664a93614e7dd20bd2ed3
SyedaFarheen1 <spongheen@gmail.com>
2025-05-01 18:39:26 +0500

    added hello FAST students

--- github-example/main.cpp
+++ github-example/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     string name;
-    cout << "Hello, World!" << endl;
+    cout << "Hello, FAST Students!" << endl;
     cout << "Hello, GitHub!" << endl;
         
     cout << "Enter your name: ";
