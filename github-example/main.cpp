522f3f2e2398ea420c56e8d6069a0b4bd66ebfbc
SyedaFarheen1 <spongheen@gmail.com>
2025-05-01 18:36:13 +0500

    added hello dunya

--- github-example/main.cpp
+++ github-example/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     string name;
-    cout << "Hello, World!" << endl;
+    cout << "Hello, Dunya!" << endl;
     cout << "Hello, GitHub!" << endl;
         
     cout << "Enter your name: ";
